dc8c8863e1834e4c7f2936821315bb064089c4e0
Viktor <webkoda@mail.ru>
2020-10-06 17:52:12 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// const result = confirm('Ты здесь?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?', 'Да мне есть 18');
+// console.log(typeof(answer));
+
+const answer = [];
+answer[0] = prompt('Введите ваше имя', '');
+answer[1] = prompt('Введите вашу фамилию', '');
+answer[2] = prompt('Сколько Вам лет', '');
+
+document.write(answer);
